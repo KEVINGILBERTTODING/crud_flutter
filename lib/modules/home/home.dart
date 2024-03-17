c1fbafe840d89c5c35d5db37a78586f76fa013e9
KEVINGILBERTTODING <122202002832@mhs.dinus.ac.id>
2024-03-17 17:17:55 +0700

    Feat: added bottom sheet

--- lib/modules/home/home.dart
+++ lib/modules/home/home.dart
@@ -1,6 +1,3 @@
-import 'dart:ffi';
-import 'dart:math';
-
 import 'package:crud_flutter/modules/products/views/list_products.dart';
 import 'package:crud_flutter/providers/auth/auth_provider.dart';
 import 'package:flutter/material.dart';
@@ -14,6 +11,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _productNameController = TextEditingController();
+  TextEditingController _productPriceController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -46,8 +47,83 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              isScrollControlled: true,
+              builder: (BuildContext context) {
+                return SingleChildScrollView(
+                    padding: EdgeInsets.only(
+                      bottom: MediaQuery.of(context).viewInsets.bottom,
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Input data baru',
+                            style:
+                                TextStyle(fontFamily: 'popbold', fontSize: 15),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          TextFormField(
+                            validator: (value) {
+                              if (value == null || value == '') {
+                                print('inputan kosong');
+                                return 'Nama produk tidak boleh kosong';
+                              }
+                              return null;
+                            },
+                            autocorrect: true,
+                            controller: _productNameController,
+                            decoration: InputDecoration(
+                                labelText: 'Nama Produk',
+                                border: OutlineInputBorder(),
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+
+                          TextField(
+                            controller: _productPriceController,
+                            decoration: InputDecoration(
+                                labelText: 'Harga Produk',
+                                border: OutlineInputBorder(),
+                                floatingLabelBehavior:
+                                    FloatingLabelBehavior.always),
+                          ),
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Align(
+                            child: TextButton(
+                                onPressed: () {
+                                  String unm = _productNameController.text;
+                                  ScaffoldMessenger.of(context)
+                                      .showSnackBar(SnackBar(
+                                    behavior: SnackBarBehavior.floating,
+                                    content: Text(unm),
+                                    duration: Duration(seconds: 2),
+                                  ));
+                                },
+                                child: Text('Simpan'),
+                                style: ButtonStyle(
+                                    backgroundColor: MaterialStateProperty.all(
+                                        Colors.amber))),
+                          )
+
+                          // Tambahkan widget lain yang diperlukan di sini
+                        ],
+                      ),
+                    ));
+              });
+        },
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
